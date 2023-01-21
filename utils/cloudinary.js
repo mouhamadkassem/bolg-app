6a204c12a997e73d36c708907de31ce4127a2e43
Mohamad <mohamadkassem101@gmail.com>
2023-01-21 14:22:09 +0200

    'deployment'

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -3,7 +3,7 @@ const cloudinary = require("cloudinary");
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_SC_KEY,
+  api_secret: process.env.CLOUDINARY_SC_KEY,
 });
 
 async function cloudinaryUploadImg(fileToUpload) {
